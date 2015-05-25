62bad302d8caca3668b3fc43e1d2e0424f2e152c
Konstantinos Koukopoulos <kouk@noc.uoa.gr>
2015-05-25 13:51:40 +0000

    add an index view

--- regapiapp.py
+++ regapiapp.py
@@ -3,6 +3,7 @@ from flask import (Flask, render_template, request, redirect, make_response,
 import jsonrpclib
 from proxyapp import app as proxyapp
 from regapi_settings import regapi_settings
+from cas_settings import cas_settings
 
 class RegAPI(object):
     def __init__(self):
@@ -14,17 +15,23 @@ class RegAPI(object):
 
 app = Flask(__name__)
 app.debug = True
-app.config['APPLICATION_ROOT'] = cas_settings['url_prefix']
+app.config['APPLICATION_ROOT'] = regapi_settings.get('url_prefix', '/regapi')
 app.config['SERVER_NAME'] = cas_settings['server_name']
 log = app.logger
+log.error("LALA")
 
 regapibp = Blueprint('regapi', __name__, template_folder='templates')
+
+@regapibp.route('/')
+def index():
+    log.debug("foo")
+    return "Hai there"
 
 @regapibp.route('/lessons/<year>/<user>')
 def get_lessons(year, user):
     lessons = RegAPI().lessons(year, user)
     return ",".join(repr(lessons))
-app.register_blueprint(regapibp, url_prefix=regapi_settings.get(url_prefix, '/regapi'))
+app.register_blueprint(regapibp, url_prefix=app.config['APPLICATION_ROOT'])
 
 from werkzeug.debug import DebuggedApplication
 debug_app = DebuggedApplication(app, evalex=True)
